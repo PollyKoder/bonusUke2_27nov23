05e131c2e125dbe01f2481ccc41745f11c623d1f
Polly <riviera.koder@gmail.com>
2023-11-27 12:35:37 +0100

    edit_LectureJS

--- scriptJulieLecture.js
+++ scriptJulieLecture.js
@@ -1,22 +1,22 @@
 
 
-// function submitTaskFive() {
-//     let taskFiveInput = document.querySelector("#taskFiveInput").value;
-//     console.log(taskFiveInput);
+function submitTaskFive() {
+    let taskFiveInput = document.querySelector("#taskFiveInput").value;
+    console.log(taskFiveInput);
 
-//     let taskFiveP = document.querySelector("#taskFiveP");
-//     taskFiveP.textContent= taskFiveInput;
-// }
+    let taskFiveP = document.querySelector("#taskFiveP");
+    taskFiveP.textContent= taskFiveInput;
+}
 
-// let taskFiveButton = document.querySelector("#taskFiveButton");
-// taskFiveButton.addEventListener("click", submitTaskFive);
+ let taskFiveButton = document.querySelector("#taskFiveButton");
+ taskFiveButton.addEventListener("click", submitTaskFive);
 
-taskFiveButton.addEventListener("click", () => {
-    function submitTaskFive() {
-        let taskFiveInput = document.querySelector("#taskFiveInput").value;
-        console.log(taskFiveInput);
+// taskFiveButton.addEventListener("click", () => {
+//     function submitTaskFive() {
+//         let taskFiveInput = document.querySelector("#taskFiveInput").value;
+//         console.log(taskFiveInput);
     
-        let taskFiveP = document.querySelector("#taskFiveP");
-        taskFiveP.textContent= taskFiveInput;
-    }
-});
+//         let taskFiveP = document.querySelector("#taskFiveP");
+//         taskFiveP.textContent= taskFiveInput;
+//     }
+// });
